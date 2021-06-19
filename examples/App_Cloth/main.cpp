cd2ea48a5abc3fbda1f45e44ae580c28e40e0fc9
clouddon <clouddon@sina.com>
2021-06-19 10:43:08 +0800

    Revert "Setup a t-shirt demo"
    This reverts commit d50eac1a96aa7ff8e268d7c23d4cd3a99f48c308.

--- examples/App_Cloth/main.cpp
+++ examples/App_Cloth/main.cpp
@@ -7,8 +7,6 @@
 
 #include "ParticleSystem/StaticBoundary.h"
 
-#include "RigidBody/RigidBody.h"
-
 #include "PointRenderModule.h"
 #include "SurfaceMeshRender.h"
 #include "ParticleCloth.h"
@@ -21,9 +19,8 @@ void CreateScene()
 	SceneGraph& scene = SceneGraph::getInstance();
 
 	std::shared_ptr<StaticBoundary<DataType3f>> root = scene.createNewScene<StaticBoundary<DataType3f>>();
-	root->loadSDF("../../data/t-shirt/body-scaled.sdf");
-// 	root->loadCube(Vec3f(-1.5, 0, -1.5), Vec3f(1.5, 3.5, 1.5), 0.05f, true);
-// 	root->loadShpere(Vec3f(0.0, 0.7f, 0.0), 0.15f, 0.005f, false, true);
+	root->loadCube(Vec3f(0), Vec3f(1), 0.005f, true);
+	root->loadShpere(Vec3f(0.5, 0.7f, 0.5), 0.08f, 0.005f, false, true);
 
 	std::shared_ptr<ParticleCloth<DataType3f>> child3 = std::make_shared<ParticleCloth<DataType3f>>();
 	root->addParticleSystem(child3);
@@ -34,24 +31,8 @@ void CreateScene()
 	child3->setVisible(false);
 
 	child3->setMass(1.0);
-//   	child3->loadParticles("../../data/cloth/cloth.obj");
-//   	child3->loadSurface("../../data/cloth/cloth.obj");
-
-	child3->loadParticles("../../data/t-shirt/t-shirt.obj");
-	child3->loadSurface("../../data/t-shirt/t-shirt.obj");
-
-	//child3->scale(0.5);
-
-	std::shared_ptr<RigidBody<DataType3f>> rigidbody = std::make_shared<RigidBody<DataType3f>>();
-	root->addRigidBody(rigidbody);
-	rigidbody->loadShape("../../data/t-shirt/body-scaled.obj");
-	rigidbody->scale(0.95);
-	rigidbody->translate(Vec3f(0, 0.07, 0));
-	rigidbody->setActive(false);
-
-	auto render = std::make_shared<SurfaceMeshRender>();
-	render->setColor(Vec3f(0.1, 1.0, 1));
-	rigidbody->getSurface()->addVisualModule(render);
+  	child3->loadParticles("../../data/cloth/cloth.obj");
+  	child3->loadSurface("../../data/cloth/cloth.obj");
 }
 
 int main()
