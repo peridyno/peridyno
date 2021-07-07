29a5855b1dd692fc605675214f98b01e93d5bfca
lxk <Herrwener@163.com>
2021-07-07 15:14:55 +0800

    Add render lighting setting

--- src/RenderEngine/RenderParams.h
+++ src/RenderEngine/RenderParams.h
@@ -42,7 +42,7 @@ namespace dyno
 		{
 			// ambient light
 			glm::vec3	ambientColor = glm::vec3(0.05f);
-			float		ambientScale = 1.f;
+			float		ambientScale = 1.f; 
 			// directional light
 			glm::vec3	mainLightColor = glm::vec3(1.f);
 			float		mainLightScale = 5.f;
@@ -51,7 +51,7 @@ namespace dyno
 			glm::mat4	mainLightVP = glm::mat4(1);
 		} light;
 
-
+		// Backcolor gray scale
 		glm::vec3	bgColor0 = glm::vec3(0.2f);
 		glm::vec3	bgColor1 = glm::vec3(0.8f);
 
