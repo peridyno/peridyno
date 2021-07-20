6d7bf57597b673a3d11201fd526d42fb9adf40fe
lxk <herrwerner@163.com>
2021-07-20 17:10:39 +0800

    Add ColorBar

--- external/imgui/imgui.h
+++ external/imgui/imgui.h
@@ -894,9 +894,11 @@ namespace ImGui
     // 水平布局
     IMGUI_API void          BeginHorizontal();
     IMGUI_API void          EndHorizontal();
-
     // Logo带字按钮
     IMGUI_API bool ImageButtonWithText(ImTextureID texId,const char* label,const ImVec2& imageSize=ImVec2(0,0), const ImVec2& uv0 = ImVec2(0,0),  const ImVec2& uv1 = ImVec2(1,1), int frame_padding = -1, const ImVec4& bg_col = ImVec4(0,0,0,0), const ImVec4& tint_col = ImVec4(1,1,1,1));
+    // ColorBar 
+    IMGUI_API bool ColorBar(const char* label, const int* values, const ImU32* col, int length);
+
 
 } // namespace ImGui
 
