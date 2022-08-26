27def9542df3f3835ade4123e2fac242f693ca65
clouddon <clouddon@sina.com>
2022-08-26 22:20:13 +0800

    Update VTK examples

--- examples/QtGUI/Qt_VtkVisualModule/main.cpp
+++ examples/QtGUI/Qt_VtkVisualModule/main.cpp
@@ -4,7 +4,8 @@
 
 #include <ParticleSystem/ParticleFluid.h>
 #include <ParticleSystem/StaticBoundary.h>
-#include <ParticleSystem/ParticleEmitterSquare.h>
+#include <ParticleSystem/SquareEmitter.h>
+#include <ParticleSystem/MakeParticleSystem.h>
 
 #include <Module/CalculateNorm.h>
 
@@ -12,6 +13,9 @@
 
 #include <VtkRenderEngine.h>
 #include <VtkFluidVisualModule.h>
+
+#include "CubeModel.h"
+#include "CubeSampler.h"
 
 #include <ImColorbar.h>
 
@@ -24,13 +28,31 @@ std::shared_ptr<SceneGraph> createScene()
 	scn->setUpperBound(Vec3f(1.5, 1, 1.5));
 	scn->setLowerBound(Vec3f(-0.5, 0, -0.5));
 
+	//Create a cube
+	auto cube = scn->addNode(std::make_shared<CubeModel<DataType3f>>());
+	cube->varLocation()->setValue(Vec3f(0.6, 0.85, 0.5));
+	cube->varLength()->setValue(Vec3f(0.1, 0.65, 0.1));
+	cube->graphicsPipeline()->disable();
+
+	//Create a sampler
+	auto sampler = scn->addNode(std::make_shared<CubeSampler<DataType3f>>());
+	sampler->varSamplingDistance()->setValue(0.005);
+	sampler->graphicsPipeline()->disable();
+
+	cube->outCube()->connect(sampler->inCube());
+
+	auto initialParticles = scn->addNode(std::make_shared<MakeParticleSystem<DataType3f>>());
+
+	sampler->statePointSet()->promoteOuput()->connect(initialParticles->inPoints());
+
+	auto fluid = scn->addNode(std::make_shared<ParticleFluid<DataType3f>>());
+	//fluid->loadParticles(Vec3f(0.5, 0.2, 0.4), Vec3f(0.7, 1.5, 0.6), 0.005);
+	initialParticles->connect(fluid->importInitialStates());
+
 	auto root = scn->addNode(std::make_shared<StaticBoundary<DataType3f>>());
 	root->loadCube(Vec3f(-0.5, 0, -0.5), Vec3f(1.5, 2, 1.5), 0.02, true);
 	root->loadSDF(getAssetPath() + "bowl/bowl.sdf", false);
-
-	auto fluid = scn->addNode(std::make_shared<ParticleFluid<DataType3f>>());
-	fluid->loadParticles(Vec3f(0.5, 0.2, 0.4), Vec3f(0.7, 1.5, 0.6), 0.005);
-	root->addParticleSystem(fluid);
+	initialParticles->connect(root->importParticleSystems());
 
 	auto calculateNorm = std::make_shared<CalculateNorm<DataType3f>>();
 	auto colorMapper = std::make_shared<ColorMapping<DataType3f>>();
--- examples/VTK/Vtk_TransparentFluid/main.cpp
+++ examples/VTK/Vtk_TransparentFluid/main.cpp
@@ -6,9 +6,13 @@
 #include <ParticleSystem/ParticleFluid.h>
 #include <RigidBody/RigidBody.h>
 #include <ParticleSystem/StaticBoundary.h>
+#include <ParticleSystem/MakeParticleSystem.h>
 
 #include <VtkRenderEngine.h>
 #include <VtkFluidVisualModule.h>
+
+#include <CubeModel.h>
+#include <CubeSampler.h>
 
 using namespace std;
 using namespace dyno;
@@ -19,13 +23,30 @@ std::shared_ptr<SceneGraph> createScene()
 	scn->setUpperBound(Vec3f(1.5, 1, 1.5));
 	scn->setLowerBound(Vec3f(-0.5, 0, -0.5));
 
+	//Create a cube
+	auto cube = scn->addNode(std::make_shared<CubeModel<DataType3f>>());
+	cube->varLocation()->setValue(Vec3f(0.6, 0.85, 0.5));
+	cube->varLength()->setValue(Vec3f(0.1, 0.65, 0.1));
+	cube->graphicsPipeline()->disable();
+
+	//Create a sampler
+	auto sampler = scn->addNode(std::make_shared<CubeSampler<DataType3f>>());
+	sampler->varSamplingDistance()->setValue(0.005);
+	sampler->graphicsPipeline()->disable();
+
+	cube->outCube()->connect(sampler->inCube());
+
+	auto initialParticles = scn->addNode(std::make_shared<MakeParticleSystem<DataType3f>>());
+
+	sampler->statePointSet()->promoteOuput()->connect(initialParticles->inPoints());
+
+	auto fluid = scn->addNode(std::make_shared<ParticleFluid<DataType3f>>());
+	//fluid->loadParticles(Vec3f(0.5, 0.2, 0.4), Vec3f(0.7, 1.5, 0.6), 0.005);
+	initialParticles->connect(fluid->importInitialStates());
+
 	auto root = scn->addNode(std::make_shared<StaticBoundary<DataType3f>>());
 	root->loadCube(Vec3f(-0.5, 0, -0.5), Vec3f(1.5, 2, 1.5), 0.02, true);
 	root->loadSDF(getAssetPath() + "bowl/bowl.sdf", false);
-
-	auto fluid = scn->addNode(std::make_shared<ParticleFluid<DataType3f>>());
-	fluid->loadParticles(Vec3f(0.5, 0.2, 0.4), Vec3f(0.7, 1.5, 0.6), 0.005);
-	root->addParticleSystem(fluid);
 
 	// point
 	auto fRender = std::make_shared<VtkFluidVisualModule>();
