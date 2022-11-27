c45b5f3509a2af2bee8f98ffea84376374ec3dcc
regnore <regnorez@gmail.com>
2022-11-28 04:18:07 +0800

    Add new picker nodes for EdgeSet and PointSet

---
+++ src/Interaction/PointPickerNode.h
@@ -0,0 +1,59 @@
+#pragma once
+#include "Node.h"
+#include "Topology/TriangleSet.h"
+#include "Module/TopologyModule.h"
+#include "SurfaceInteraction.h"
+#include "EdgeInteraction.h"
+#include "PointInteraction.h"
+
+namespace dyno
+{
+	template<typename TDataType>
+	class PointPickerNode : public Node
+	{
+		DECLARE_TCLASS(PointPickerNode, TDataType)
+	public:
+		typedef typename TDataType::Real Real;
+		typedef typename TDataType::Coord Coord;
+
+		DEF_INSTANCE_IN(PointSet<TDataType>, Topology, "");
+
+		DEF_ARRAY_STATE(int, PointIndex, DeviceType::GPU, "");
+
+		DECLARE_ENUM(PickingTypeSelection,
+		Click = 0,
+			Drag = 1,
+			Both = 2
+			);
+
+		DEF_ENUM(PickingTypeSelection, PickingType, PickingTypeSelection::Both, "");
+
+		DECLARE_ENUM(MultiSelectionType,
+		OR = 0,
+			XOR = 1,
+			C = 2
+			);
+
+		DEF_ENUM(MultiSelectionType, MultiSelectionType, MultiSelectionType::OR, "");
+
+		DEF_VAR(Real, InterationRadius, 0.002f, "The radius of interaction");
+		DEF_VAR(Real, PointSelectedSize, 0.012f, "");
+		DEF_VAR(Real, PointOtherSize, 0.01f, "");
+
+		DEF_VAR(Vec3f, SelectedPointColor, Vec3f(1.0f, 0, 0), "");
+		DEF_VAR(Vec3f, OtherPointColor, Vec3f(0, 0, 1.0f), "");
+
+		PointPickerNode(std::string name = "default");
+		~PointPickerNode();
+
+		std::string getNodeType();
+
+		void resetStates() override;
+
+		void changePickingType();
+		void changeMultiSelectionType();
+
+	private:
+		std::shared_ptr<PointInteraction<TDataType>> pointInteractor;
+	};
+}
