750cd0a68cacc61726460924ccc5df80666eb7a6
YuzhongGuo <1345656488@qq.com>
2023-07-27 17:01:52 +0800

    modify QRampWidget
    # Conflicts:
    #	src/Rendering/GUI/QtGUI/PropertyItem/QRampWidget.cpp
    #	src/Rendering/GUI/QtGUI/PropertyItem/QRampWidget.h

--- src/Rendering/GUI/QtGUI/PropertyItem/QRampWidget.h
+++ src/Rendering/GUI/QtGUI/PropertyItem/QRampWidget.h
@@ -18,22 +18,129 @@
  */
 #pragma once
 #include "QFieldWidget.h"
-
+#include <QKeyEvent>
+#include <QEvent>
 #include "Ramp.h"
+#include "PCustomWidgets.h"
+#include "QBoolFieldWidget.h"
+#include "qcheckbox.h"
+#include <algorithm>
+#include <QPainterPath>
+#include "qlayout.h"
 
 namespace dyno
 {
+
+
 	class QRampWidget : public QFieldWidget
 	{
 		Q_OBJECT
 	public:
 		QRampWidget(FBase* field);
 		~QRampWidget() override {};
+	
+
 
 	public slots:
 		void updateField();
 
-	private:
+	};
+
+	class mQDoubleSlider : public QDoubleSlider
+	{
+		Q_OBJECT
+
+	public:
+
+		mQDoubleSlider(QWidget* pParent = NULL) {};
+
+		int id = -1;
+
+	public:
+		QLabel* nameLabel = nullptr;
+
+	public slots:
+
+		void setNewVisable(int s)
+		{
+			this->setVisible(bool(s));
+			if (this->nameLabel != nullptr) 
+			{
+				this->nameLabel->setVisible(bool(s));
+			}
+		}
+	};
+
+	class mQDoubleSpinner : public QDoubleSpinner
+	{
+		Q_OBJECT
+
+	public:
+
+		mQDoubleSpinner(QWidget* pParent = NULL) 
+		{
+			connect(this, SIGNAL(valueChanged(double)), this, SLOT(setValue(double)));
+		}
+
+	signals:
+		void valueChangedAndID(double Value, int i);
+
+	public slots:
+
+		void setValue(double Value, bool BlockSignals = false) 
+		{
+
+			if (!BlockSignals) { emit valueChangedAndID(Value, id); }
+			QDoubleSpinner::setValue(Value,BlockSignals);
+
+		}
+
+		void setNewVisable(int s)
+		{
+			this->setVisible(bool(s));
+		}
+
+
+	public:
+		int id = -1;
+	};
+
+	class mQCheckBox : public QCheckBox
+	{
+		Q_OBJECT
+
+	public:
+
+		mQCheckBox(QWidget* pParent = NULL)
+		{
+			connect(this, SIGNAL(stateChanged(int)), this, SLOT(findSignal(int)));
+			
+		}
+
+	signals:
+
+		void mValueChanged(int);
+
+	public slots:
+
+		void findSignal(int i)
+		{
+			 emit mValueChanged(i); 
+
+		}
+
+		void setNewVisable(int s)
+		{
+			this->setVisible(!bool(s));
+			if (this->nameLabel != nullptr)
+			{
+				this->nameLabel->setVisible(!bool(s));
+			}
+		}
+
+
+	public:
+		QLabel* nameLabel = nullptr;
 	};
 
 	class QDrawLabel : public QWidget
@@ -49,15 +156,65 @@ namespace dyno
 				this->x = a;
 				this->y = b;
 			}
+			void set(MyCoord s)
+			{
+				this->x = s.x;
+				this->y = s.y;
+			}
+			MyCoord (int a, int b) {
+				this->x = a;
+				this->y = b;
+			}
+
+			MyCoord (Vec2f s) {
+				this->x = s[0];
+				this->y = s[1];
+			}
+			MyCoord() {};
+
+			bool operator == (MyCoord s ) 
+			{
+				if (this->x == s.x && this->y == s.y) 
+				{
+					return true;
+				}
+				return false;
+			}
+			MyCoord operator - (MyCoord s)
+			{
+				MyCoord a;
+				a.x = this->x -= s.x;
+				a.y = this->y -= s.y;
+				return a;
+			}
+			MyCoord operator + (MyCoord s)
+			{
+				MyCoord a;
+				a.x = this->x += s.x;
+				a.y = this->y += s.y;
+				return a;
+			}
+
 		};
 		struct Coord0_1
 		{
-			float x = 0;
-			float y = 0;
-			void set(float a, float b) 
+			double x = 0;
+			double y = 0;
+			void set(double a, double b)
 			{
 				this->x = a;
 				this->y = b;
+			}
+		};
+		struct EndPoint
+		{
+			int firstPoint = -1;
+			int secondPoint = -1;
+			EndPoint() {};
+			EndPoint(int f, int s)
+			{
+				firstPoint = f;
+				secondPoint = s;
 			}
 		};
 
@@ -66,6 +223,11 @@ namespace dyno
 			x = 0,
 			y = 1,
 		};
+		enum Interpolation 
+		{
+			Linear = 0,
+			Bezier = 1,
+		};
 
 		enum BorderMode
 		{
@@ -73,65 +235,33 @@ namespace dyno
 			Close = 1,
 		};
 
-		enum RectShape
-		{
-			Rect = 0,
-			Squard= 1,
-
-		};
-
-	public:
-		QDrawLabel(QWidget* parent = nullptr);
-		~QDrawLabel();
-		Dir setMode(int s) { return Dir(s); }
-		void setBorderMode(int border) { if (border == 0) {borderMode = Open;}else{borderMode = Close;}}
-		void setField(FVar<Ramp>* f) { field = f; }
-		void copyFromField(std::vector<Ramp::MyCoord2D> coord01) 
-		{
-			auto ff = field->getValue();
-			if (coord01.size()) 
-			{
-				for (auto it : coord01)
-				{
-					Coord0_1 s;
-					s.set(it.x, it.y);
-					reSortCoordArray.push_back(ZeroOneToCoord(s,ff.oMinX, ff.oMaxX, ff.oMinY, ff.oMaxY));
-					//printf("RCApush值：%d\n", ZeroOneToCoord(s, ff->oMinX, ff->oMaxX, ff->oMinY, ff->oMaxY).x, ZeroOneToCoord(s, ff->oMinX, ff->oMaxX, ff->oMinY, ff->oMaxY).y);
-				}
-				printf("拷贝的RCA大小%d\n", reSortCoordArray.size());
-
-				CoordArray.push_back(reSortCoordArray[0]);
-				CoordArray.push_back(reSortCoordArray[reSortCoordArray.size()-1]);
-				for (size_t i = 1;i < reSortCoordArray.size()-1;i++)
-				{
-					CoordArray.push_back(reSortCoordArray[i]);
-
-
-				}
-			}
-			printf("拷贝的CA大小%d\n",CoordArray.size());
-			update();
-		}
-		void copyFromField(std::vector<Ramp::OriginalCoord> coord01)
-		{
-			auto ff = field->getValue();
-			if (coord01.size())
-			{
-				for (auto it : coord01)
-				{
-					MyCoord s;
-					s.set(it.x, it.y);
-					CoordArray.push_back(s);
-					//printf("RCApush值：%d\n", ZeroOneToCoord(s, ff->oMinX, ff->oMaxX, ff->oMinY, ff->oMaxY).x, ZeroOneToCoord(s, ff->oMinX, ff->oMaxX, ff->oMinY, ff->oMaxY).y);
-				}
-				
-			}
-			printf("拷贝的CA大小%d\n", CoordArray.size());
-			update();
-		}
-
 	public slots:
 		void changeValue(int s);
+		void changeInterpValue(int s);
+		void changeLabelSize();
+		void setCurveClose();
+		void updateField() {};
+		void setSpacingToDrawLabel(double value, int id);
+		void SetValueToDrawLabel(double value, int id);
+		void setLinearResample(int s);
+		void setUseRamp(int v);
+
+	public:
+
+		QDrawLabel(QWidget* parent = nullptr);
+		QDrawLabel(FVar<Ramp>* f,QWidget* parent = nullptr);
+		~QDrawLabel();
+		void updateDataToField();
+		Dir setMode(int s) { return Dir(s); }
+		void setBorderMode(int border) { if (border == 0) {borderMode = Open;}else{borderMode = Close;}}
+		void setField(FVar<Ramp>* f) 
+		{
+			field = TypeInfo::cast<FVar<Ramp>>(f);
+		}
+		void copyFromField(std::vector<Ramp::Coord2D> coord01, std::vector<MyCoord>& thisArray);
+		void copyFromField(std::vector<Ramp::OriginalCoord> coord01, std::vector<MyCoord>& thisArray);
+		void updateLabelShape();
+		void copySettingFromField();
 
 	protected:
 		void paintEvent(QPaintEvent* event);
@@ -140,46 +270,23 @@ namespace dyno
 		void mouseReleaseEvent(QMouseEvent* event)override;
 		void reSort(std::vector<MyCoord>& vector1);
 		void initializeLine(Dir mode );
-		void updateFloatCoordArray() { floatCoord.clear(); for (auto it : reSortCoordArray) { floatCoord.push_back(CoordTo0_1Value(it)); } }
-		void CoordtoField(std::vector<Coord0_1> CA, FVar<Ramp>* f) 
-		{	
-			auto s = f->getValue();
-			s.clearMyCoord();
-			for (auto it : CA) 
-			{
-				s.addItemMyCoord(it.x,it.y);
-
-			}
-			for (auto it : CoordArray)
-			{
-				s.addItemOriginalCoord(it.x, it.y);
-			}
-			s.setOriginalCoord(minX,maxX,minY,maxY);
-		}
-		Coord0_1 CoordTo0_1Value(MyCoord& coord) 
-		{//曲线坐标转换到0-1浮点值，并反转Y轴
-			Coord0_1 s;
-			float x = float(coord.x);
-			float y = float(coord.y);
-			float fmaxX = float(maxX);
-			float fminX = float(minX);
-			float fmaxY = float(maxY);
-			float fminY = float(minY);
-
-			s.x = (x - fminX) / (fmaxX - fminX);
-			s.y = 1 - (y - fminY) / (fmaxY - fminY);
-			return s;
-		}
-		MyCoord ZeroOneToCoord(Coord0_1& value, int x0,int x1,int y0, int y1) 
-		{//0-1浮点值到曲线坐标
-			MyCoord s;
-			printf("x0: %d x1: %d y0: %d y1: %d\n", x0, x1, y0, y1);
-			s.x = int(value.x * float(x1 - x0)) + x0;
-			s.y = int((1-value.y) * float(y1 - y0)) + y0;
-			std::cout << value.x << " --- " << value.y << std::endl;
-			std::cout << s.x << " *** " << s.y << std::endl;
-			return s;
-		}
+		void deletePoint();
+		void setLabelSize(int minX , int minY, int maxX, int maxY);
+		void buildBezierPoint();
+		void keyPressEvent(QKeyEvent* event);
+		int addPointtoEnd();
+		int insertCurvePoint(MyCoord pCoord);
+		void insertHandlePoint(int fp, MyCoord pCoord);	
+		void keyReleaseEvent(QKeyEvent* event);
+		void leaveEvent(QEvent* event);
+		void remapArrayToHeight(std::vector<MyCoord>& Array,int h);
+		void updateFloatCoordArray(std::vector<MyCoord> CoordArray, std::vector<Coord0_1>& myfloatCoord);
+		void CoordtoField(Ramp& s);
+		Coord0_1 CoordTo0_1Value(MyCoord& coord);
+		MyCoord ZeroOneToCoord(Coord0_1& value, int x0, int x1, int y0, int y1);
+		void buildCoordToResortMap();
+		void buildHandlePointSet();
+		void insertElementToHandlePointSet(int i);
 
 		static bool sortx(MyCoord a, MyCoord b)
 		{
@@ -191,32 +298,101 @@ namespace dyno
 			if (a.y < b.y) { return true; }
 			else { return false; }
 		};
-		
-		std::vector<MyCoord> CoordArray;
-		std::vector<MyCoord> reSortCoordArray;
-		MyCoord hoverCoord;
-		MyCoord selectCoord;
-		std::vector<Coord0_1> floatCoord;
+
+
+
+	public:
+		//Setting
+		bool useBezier = false;
+		bool isSquard = false;
+		bool useSort = true;
+		bool lockSize = false;
+		bool curveClose = false;
+		bool LineResample = false;
+		bool useRamp = false;
+		double spacing = 10;				//Resampling Spacing
+		BorderMode borderMode = Close;		//Ramp Or Canvas
 
 	private:
-		int w0;
-		int h0;
-		int currentW;
-		int currentH;
-		Dir Mode = x;
-		BorderMode borderMode = Close;
-		int hoverPoint = -1;
-		int selectPoint = -1;
-		bool isSelect = false;
-		bool isHover = false;
+
+		//Default Setting
+		int w = 380;						//Default Width
+		int h = 100;						//Default Height
+
+		int selectDistance = 10;			//SelectDistanceThreshold
+		int HandleAngleThreshold = 20;  
+		int radius = 4;						//Point Radius
+		float iniHandleLength = 15;			//DefaultHandleLength
+
+		Dir Mode = x;						//Direction for CloseMode
+		Interpolation InterpMode= Linear;   //Interpolation
+		FVar<Ramp>* field;					//field
+
+		//reSize
 		int maxX = 0;
 		int maxY = 0;
 		int minX = 0;
 		int minY = 0;
-		int selectDistance = 10;
-		int radius = 4;
-		FVar<Ramp>* field;
+		double NmaxX = 0;
+		double NmaxY = 0;
+		double NminX = 0;
+		double NminY = 0;
+		bool generatorXmin = 1;
+		bool generatorXmax = 1;
+		bool generatorYmin = 1;
+		bool generatorYmax = 1;
 
+		//Temp
+		int minIndex = 0;
+		int maxIndex = 0;
+
+		//KeyboardStatus
+		bool altKey = false;
+		bool ctrlKey = false;
+		bool shiftKey = false;
+
+		//CurrentStatus
+		int hoverPoint = -1;
+		int selectHandlePoint = -1;
+		int selectPoint = -1;
+		bool isSelect = false;
+		bool isHandleSelect = false;
+		int handleParent = -1;
+		int selectPointInResort = -1;
+		bool isHover = false;
+		int connectHandlePoint = -1;
+		float connectLength;
+		bool ForceUpdate = false;
+		bool InsertBezierOpenPoint = false;
+		bool HandleConnection = true;
+		bool InsertAtBegin = false;
+
+		//Coord
+		MyCoord iniPos;					//initial position of the SelectedPoint
+		MyCoord dtPos;					//delta position of th SelectedPoint
+		MyCoord iniHandlePos;			//initial position of the SelectedHandlePoint
+		MyCoord pressCoord;				//Current PressCoord
+		MyCoord hoverCoord;				
+		MyCoord selectCoord;
+
+		//CurveCoord
+		std::vector<MyCoord> CoordArray;			//CoordArray by Press
+		std::vector<MyCoord> reSortCoordArray;		//Reorder Coordarray
+		std::vector<MyCoord> HandlePoints;			//handlePoints by CoordArray
+		std::vector<Coord0_1> floatCoord;			// updateFloatCoordArray(reSortCoordArray, floatCoord); 
+		std::vector<Coord0_1> bezierFloatCoord;		// updateFloatCoordArray(CurvePoint, bezierFloatCoord); 
+		std::vector<Coord0_1> handleFloatCoord;
+		std::vector<MyCoord> CurvePoint;			//	resample Path
+		std::vector<int> multiSelectID;				
+
+		//Map
+		std::map<int, float> curvePointMapLength;		//find Length by curvePoint
+		std::map<float, EndPoint> lengthMapEndPoint;	//find EndPoint by Length
+		std::map<int, int> mapOriginalIDtoResortID;		//find reSortArray id by CoordArray id
+		std::map<int, int> mapResortIDtoOriginalID;     //find CoordArray id by reSortArray id
+
+		//Path
+		QPainterPath path;
 	};
 
 
