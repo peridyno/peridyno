f2e29876bcc46284cad9e5828106247740d1fb1b
clouddon <clouddon@sina.com>
2024-01-08 15:34:32 +0800

    Add a height map

--- examples/Cuda/HeightField/GL_Terrain/main.cpp
+++ examples/Cuda/HeightField/GL_Terrain/main.cpp
@@ -17,7 +17,7 @@ std::shared_ptr<SceneGraph> createScene()
 	std::shared_ptr<SceneGraph> scn = std::make_shared<SceneGraph>();
 
 	auto land = scn->addNode(std::make_shared<LandScape<DataType3f>>());
-	land->varFileName()->setValue(getAssetPath() + "landscape/Landscape_1 Map_1024x1024.png");
+	land->varFileName()->setValue(getAssetPath() + "landscape/Landscape_1_Map_1024x1024.png");
 	land->varLocation()->setValue(Vec3f(0.0f, 100.0f, 0.0f));
 	land->varScale()->setValue(Vec3f(1.0f, 64.0f, 1.0f));
 
