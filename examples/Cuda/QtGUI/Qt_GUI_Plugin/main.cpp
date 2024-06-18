39a0403baa79b0bc408fba8f191c26a2831ece33
dreliveam <shushushu.2008@163.com>
2024-06-18 19:24:20 +0800

    Add "Dual Particle System" and "Triangular Mesh Boundary" to Toolbar.

--- examples/Cuda/QtGUI/Qt_GUI_Plugin/main.cpp
+++ examples/Cuda/QtGUI/Qt_GUI_Plugin/main.cpp
@@ -3,11 +3,11 @@ using namespace dyno;
 
 #include "RigidBody/initializeRigidBody.h"
 #include "ParticleSystem/initializeParticleSystem.h"
+#include "DualParticleSystem/initializeDualParticleSystem.h"
 #include "Peridynamics/initializePeridynamics.h"
 #include "SemiAnalyticalScheme/initializeSemiAnalyticalScheme.h"
 #include "Volume/initializeVolume.h"
 #include "Multiphysics/initializeMultiphysics.h"
-
 #include "initializeModeling.h"
 #include "initializeIO.h"
 
@@ -20,6 +20,7 @@ int main()
 	Modeling::initStaticPlugin();
 	RigidBody::initStaticPlugin();
 	PaticleSystem::initStaticPlugin();
+	DualParticleSystem::initStaticPlugin();
 	Peridynamics::initStaticPlugin();
 	SemiAnalyticalScheme::initStaticPlugin();
 	Volume::initStaticPlugin();
