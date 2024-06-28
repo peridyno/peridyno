320d910d8e7f85de12810c96a5d7132083018d22
clouddon <clouddon@sina.com>
2024-06-28 22:02:38 +0800

    Delete data

--- data/python_web_sample/GL_GlfwGUI.py
+++
@@ -1,3 +0,0 @@
-import PyPeridyno as dyno
-
-scene = dyno.SceneGraph()
--- data/python_web_sample/Qt_GLTF.py
+++
@@ -1,7 +0,0 @@
-import PyPeridyno as dyno
-
-scene = dyno.SceneGraph()
-gltf = dyno.GltfLoader3f()
-gltf.var_file_name().set_value(dyno.FilePath(dyno.get_asset_path() + "Jeep/JeepGltf/jeep.gltf"))
-
-scene.add_node(gltf)
