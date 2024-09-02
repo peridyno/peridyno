d8b7b62794dd57311990036c267e8a24b6e11e47
unibeam98 <beyondevery@live.com>
2024-09-02 17:30:31 +0800

    should add node pos

--- src/Rendering/GUI/WtGUI/NodeEditor/WtFlowScene.cpp
+++ src/Rendering/GUI/WtGUI/NodeEditor/WtFlowScene.cpp
@@ -14,24 +14,10 @@ WtFlowScene::WtFlowScene()
 {
 }
 
-WtFlowScene::~WtFlowScene() {}
-
-//std::shared_ptr<WtConnection> createConnection(PortType connectedPort,
-//	WtNode& node,
-//	PortIndex portIndex)
-//{
-//	auto connection = std::make_shared<WtConnection>(connectedPort, node, portIndex);
-//
-//	//auto cgo = detail::make_unique<WtConnectionGraphicsObject>(*this, *connection);
-//}
-//
-//std::shared_ptr<WtConnection> createConnection(WtNode& nodeIn,
-//	PortIndex portIndexIn,
-//	WtNode& nodeOut,
-//	PortIndex portIndexOut,
-//	TypeConverter const& converter = TypeConverter{})
-//{
-//}
+WtFlowScene::~WtFlowScene()
+{
+	clearScene();
+}
 
 WtNode& WtFlowScene::createNode(std::unique_ptr<WtNodeDataModel>&& dataModel, Wt::WPainter* painter)
 {
@@ -46,32 +32,6 @@ WtNode& WtFlowScene::createNode(std::unique_ptr<WtNodeDataModel>&& dataModel, Wt
 	return *nodePtr;
 }
 
-//QtNode&
-//	QtFlowScene::
-//	restoreNode(QJsonObject const& nodeJson)
-//{
-//	QString modelName = nodeJson["model"].toObject()["name"].toString();
-
-//	auto dataModel = registry().create(modelName);
-
-//	if (!dataModel)
-//		throw std::logic_error(std::string("No registered model with name ") +
-//			modelName.toLocal8Bit().data());
-
-//	auto node = detail::make_unique<QtNode>(std::move(dataModel));
-//	auto ngo = detail::make_unique<QtNodeGraphicsObject>(*this, *node);
-//	node->setGraphicsObject(std::move(ngo));
-
-//	node->restore(nodeJson);
-
-//	auto nodePtr = node.get();
-//	_nodes[node->id()] = std::move(node);
-
-//	nodePlaced(*nodePtr);
-//	nodeCreated(*nodePtr);
-//	return *nodePtr;
-//}
-
 WtDataModelRegistry& WtFlowScene::registry() const
 {
 	return *_registry;
@@ -82,26 +42,125 @@ void WtFlowScene::setRegistry(std::shared_ptr<WtDataModelRegistry> registry)
 	_registry = std::move(registry);
 }
 
+void WtFlowScene::iterateOverNodes(std::function<void(WtNode*)> const& visitor)
+{
+	for (const auto& _node : _nodes)
+	{
+		visitor(_node.second.get());
+	}
+}
+
+void WtFlowScene::iterateOverNodeData(std::function<void(WtNodeDataModel*)> const& visitor)
+{
+	for (const auto& _node : _nodes)
+	{
+		visitor(_node.second->nodeDataModel());
+	}
+}
+
+void WtFlowScene::iterateOverNodeDataDependentOrder(std::function<void(WtNodeDataModel*)> const& visitor)
+{
+	std::set<boost::uuids::uuid> visitedNodesSet;
+
+	auto isNodeLeaf = [](WtNode const& node, WtNodeDataModel const& model)
+		{
+			for (unsigned int i = 0; i < model.nPorts(PortType::In); ++i)
+			{
+				auto connections = node.nodeState().connections(PortType::In, i);
+				if (!connections.empty())
+				{
+					return false;
+				}
+			}
+			return true;
+		};
+
+	//Iterate over "leaf" nodes
+	for (auto const& _node : _nodes)
+	{
+		auto const& node = _node.second;
+		auto model = node->nodeDataModel();
+
+		if (isNodeLeaf(*node, *model))
+		{
+			visitor(model);
+			visitedNodesSet.insert(node->id());
+		}
+	}
+
+	auto areNodeInputsVisitedBefore =
+		[&](WtNode const& node, WtNodeDataModel const& model)
+		{
+			for (size_t i = 0; i < model.nPorts(PortType::In); ++i)
+			{
+				auto connections = node.nodeState().connections(PortType::In, i);
+
+				for (auto& conn : connections)
+				{
+					if (visitedNodesSet.find(conn.second->getNode(PortType::Out)->id()) == visitedNodesSet.end())
+					{
+						return false;
+					}
+				}
+			}
+
+			return true;
+		};
+
+	//Iterate over dependent nodes
+	while (_nodes.size() != visitedNodesSet.size())
+	{
+		for (auto const& _node : _nodes)
+		{
+			auto const& node = _node.second;
+			if (visitedNodesSet.find(node->id()) != visitedNodesSet.end())
+				continue;
+
+			auto model = node->nodeDataModel();
+
+			if (areNodeInputsVisitedBefore(*node, *model))
+			{
+				visitor(model);
+				visitedNodesSet.insert(node->id());
+			}
+		}
+	}
+}
+
+Wt::WPointF WtFlowScene::getNodePosition(const WtNode& node) const
+{
+	//return node.nodeGraphicsObject().pos();
+}
+
+Wt::WPointF WtFlowScene::setNodePosition(WtNode& node, const Wt::WPointF& pos) const
+{
+	node.nodeGraphicsObject().setPos(pos);
+	node.nodeGraphicsObject().moveConnections();
+}
+
 void WtFlowScene::removeNode(WtNode& node)
 {
+	// call signal
 	//nodeDeleted(node);
 	clearNode(node);
 }
 
 void WtFlowScene::clearNode(WtNode& node)
 {
-	//for (auto portType : { PortType::In, PortType::Out })
-	//{
-	//	auto nodeState = node.nodeState();
-	//	auto const& nodeEntries = nodeState.getEntries(portType);
+	for (auto portType : { PortType::In, PortType::Out })
+	{
+		auto nodeState = node.nodeState();
+		auto const& nodeEntries = nodeState.getEntries(portType);
 
-	//	for (auto& connections : nodeEntries)
-	//	{
-	//		for (auto const& pair : connections)
-	//			deleteConnection(*pair.second);
-	//	}
-	//}
-	//_nodes.erase(node.id());
+		for (auto& connections : nodeEntries)
+		{
+			for (auto const& pair : connections)
+			{
+				deleteConnection(*pair.second);
+			}
+		}
+	}
+	_nodes.erase(node.id());
 }
 
 void WtFlowScene::clearScene()
