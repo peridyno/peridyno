6f3279a90ea3ab27e1cc5b5223b9ab55e65c5507
clouddon <clouddon@sina.com>
2025-05-09 21:50:40 +0800

    Remove PyPeridyno

--- python/PyCommon.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <pybind11/pybind11.h>
-#include <pybind11/stl.h>
-#include <pybind11/operators.h>
-#include <pybind11/numpy.h>
-#include <pybind11/stl_bind.h>
-
-namespace py = pybind11;
--- python/PyObjIO.cpp
+++
@@ -1,9 +0,0 @@
-#include "PyObjIO.h"
-
-void pybind_objIO(py::module& m)
-{
-	declare_Obj_exporter<dyno::DataType3f>(m, "3f");
-	declare_Obj_loader<dyno::DataType3f>(m, "3f");
-	declare_Obj_point<dyno::DataType3f>(m, "3f");
-	declare_PlyExporter<dyno::DataType3f>(m, "3f");
-}
