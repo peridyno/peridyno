d398d90487c386a158a14640f37b8e4274a9fdd2
GuoYuzhong <1345656488@qq.com>
2025-07-11 14:27:58 +0800

    fix QPiecewiseSpinBox

--- src/Rendering/GUI/QtGUI/PropertyItem/QPiecewiseSpinBox.h
+++ src/Rendering/GUI/QtGUI/PropertyItem/QPiecewiseSpinBox.h
@@ -66,7 +66,7 @@ namespace dyno
 
 		QValidator::State validate(QString& input, int& pos) const override
 		{
-			return QValidator::Intermediate;
+			//return QValidator::Intermediate;
 			if (input.isEmpty())
 				return QValidator::Intermediate;
 
