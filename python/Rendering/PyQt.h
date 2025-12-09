9d66f3ff14ed3d4a00117d1bb819c7dff48bde48
clouddon <clouddon@sina.com>
2025-12-09 13:21:27 +0800

    Remove pyperidyno

--- python/Rendering/PyQt.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "../PyCommon.h"
-
-#include "QtGUI/QtApp.h"
-
-void declare_qt_app(py::module& m);
-
-void pybind_qt_gui(py::module& m);
--- python/Rendering/PyRenderCore.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "../PyCommon.h"
-#include "SceneGraph.h"
-#include "Field/Color.h"
-
-void declare_orbit_camera(py::module& m);
-
-void pybind_render_core(py::module& m);
