3b59abf0296d363add573445b5a2072c014d27f2
GuoYuzhong <1345656488@qq.com>
2026-03-10 15:44:44 +0800

    Fix Qt_ConfigurationVehicle

--- examples/Cuda/RigidBody/Qt_ConfigurationVehicle/main.cpp
+++ examples/Cuda/RigidBody/Qt_ConfigurationVehicle/main.cpp
@@ -39,49 +39,47 @@ std::shared_ptr<SceneGraph> creatCar()
 
 	auto configCar = scn->addNode(std::make_shared<ConfigurableBody<DataType3f>>());
 
-	auto gltf = scn->addNode(std::make_shared<GltfLoader<DataType3f>>());
-	gltf->varFileName()->setValue(getAssetPath() + "Jeep/JeepGltf/jeep.gltf");
-	gltf->setVisible(false);
+	//auto gltf = scn->addNode(std::make_shared<GltfLoader<DataType3f>>());
+	//gltf->varFileName()->setValue(getAssetPath() + "Jeep/JeepGltf/jeep.gltf");
+	//gltf->setVisible(false);
 
-	gltf->stateTextureMesh()->connect(configCar->inTextureMesh());
-
-
-	MultiBodyBind configData;
-
-	Vec3f angle = Vec3f(0, 0, 90);
-	Quat<Real> q = Quat<Real>(angle[2] * M_PI / 180, angle[1] * M_PI / 180, angle[0] * M_PI / 180);
-	;
-	configData.mVehicleRigidBodyInfo.push_back(VehicleRigidBodyInfo(Name_Shape("LF", 0), 0, Capsule, Transform3f(Vec3f(0), q.toMatrix3x3(), Vec3f(1))));//
-	configData.mVehicleRigidBodyInfo.push_back(VehicleRigidBodyInfo(Name_Shape("LB", 1), 1, Capsule, Transform3f(Vec3f(0), q.toMatrix3x3(), Vec3f(1))));
-	configData.mVehicleRigidBodyInfo.push_back(VehicleRigidBodyInfo(Name_Shape("RF", 2), 2, Capsule, Transform3f(Vec3f(0), q.toMatrix3x3(), Vec3f(1))));
-	configData.mVehicleRigidBodyInfo.push_back(VehicleRigidBodyInfo(Name_Shape("RB", 3), 3, Capsule, Transform3f(Vec3f(0), q.toMatrix3x3(), Vec3f(1))));
-	configData.mVehicleRigidBodyInfo.push_back(VehicleRigidBodyInfo(Name_Shape("BackWheel", 4), 4, Box));
-	configData.mVehicleRigidBodyInfo.push_back(VehicleRigidBodyInfo(Name_Shape("Body", 5), 5, Box));
-
-	for (size_t i = 0; i < 4; i++)
-	{
-		configData.mVehicleRigidBodyInfo[i].capsuleLength = 0.3;
-	}
-
-	configData.mVehicleJointInfo.push_back(VehicleJointInfo(Name_Shape("LF", 0), Name_Shape("Body", 5), Hinge, Vec3f(1, 0, 0), Vec3f(0), true, 10));
-	configData.mVehicleJointInfo.push_back(VehicleJointInfo(Name_Shape("LB", 1), Name_Shape("Body", 5), Hinge, Vec3f(1, 0, 0), Vec3f(0), true, 10));
-	configData.mVehicleJointInfo.push_back(VehicleJointInfo(Name_Shape("RF", 2), Name_Shape("Body", 5), Hinge, Vec3f(1, 0, 0), Vec3f(0), true, 10));
-	configData.mVehicleJointInfo.push_back(VehicleJointInfo(Name_Shape("RB", 3), Name_Shape("Body", 5), Hinge, Vec3f(1, 0, 0), Vec3f(0), true, 10));
-	configData.mVehicleJointInfo.push_back(VehicleJointInfo(Name_Shape("BackWheel", 4), Name_Shape("Body", 5), Fixed, Vec3f(1, 0, 0), Vec3f(0), true, 0));
+	//gltf->stateTextureMesh()->connect(configCar->inTextureMesh());
 
 
-	configCar->varVehicleConfiguration()->setValue(configData);
+	//MultiBodyBind configData;
 
-	configCar->varRotation()->setValue(Vec3f(0, 45, 0));
+	//Vec3f angle = Vec3f(0, 0, 90);
+	//Quat<Real> q = Quat<Real>(angle[2] * M_PI / 180, angle[1] * M_PI / 180, angle[0] * M_PI / 180);
+	//;
+	//configData.rigidBodyConfigs.push_back(RigidBodyConfig(NameRigidID("LF", 0), 0, CONFIG_SPHERE));//
+	//configData.rigidBodyConfigs.push_back(RigidBodyConfig(NameRigidID("LB", 1), 1, CONFIG_SPHERE));//
+	//configData.rigidBodyConfigs.push_back(RigidBodyConfig(NameRigidID("RF", 2), 2, CONFIG_SPHERE));//
+	//configData.rigidBodyConfigs.push_back(RigidBodyConfig(NameRigidID("RB", 3), 3, CONFIG_SPHERE));//
+	//configData.rigidBodyConfigs.push_back(RigidBodyConfig(NameRigidID("BackWheel", 4), 4, CONFIG_SPHERE));//
+	//configData.rigidBodyConfigs.push_back(RigidBodyConfig(NameRigidID("Body", 5), 5, CONFIG_BOX));//
+
+
+	//configData.jointConfigs.push_back(MultiBodyJointConfig(NameRigidID("LF", 0), NameRigidID("Body", 5), CONFIG_Hinge, Vec3f(1, 0, 0), Vec3f(0), true, 10));
+	//configData.jointConfigs.push_back(MultiBodyJointConfig(NameRigidID("LB", 1), NameRigidID("Body", 5), CONFIG_Hinge, Vec3f(1, 0, 0), Vec3f(0), true, 10));
+	//configData.jointConfigs.push_back(MultiBodyJointConfig(NameRigidID("RF", 2), NameRigidID("Body", 5), CONFIG_Hinge, Vec3f(1, 0, 0), Vec3f(0), true, 10));
+	//configData.jointConfigs.push_back(MultiBodyJointConfig(NameRigidID("RB", 3), NameRigidID("Body", 5), CONFIG_Hinge, Vec3f(1, 0, 0), Vec3f(0), true, 10));
+	//configData.jointConfigs.push_back(MultiBodyJointConfig(NameRigidID("BackWheel", 4), NameRigidID("Body", 5), CONFIG_Fixed, Vec3f(1, 0, 0), Vec3f(0), true, 0));
+
+
+	//configCar->varConfiguration()->setValue(configData);
+
+	//configCar->varRotation()->setValue(Vec3f(0, 45, 0));
 
 
 
-	std::vector<Transform3f> vehicleTransforms;
+	//std::vector<Transform3f> vehicleTransforms;
 
-	vehicleTransforms.push_back(Transform3f(Vec3f(-1,0,0), Quat1f(0, Vec3f(0, 1, 0)).toMatrix3x3()));
-	vehicleTransforms.push_back(Transform3f(Vec3f(5, 0.5, -1), Quat1f(M_PI, Vec3f(0, 1, 0)).toMatrix3x3()));
+	//vehicleTransforms.push_back(Transform3f(Vec3f(-1,0,0), Quat1f(0, Vec3f(0, 1, 0)).toMatrix3x3()));
+	//vehicleTransforms.push_back(Transform3f(Vec3f(5, 0.5, -1), Quat1f(M_PI, Vec3f(0, 1, 0)).toMatrix3x3()));
+	//vehicleTransforms.push_back(Transform3f(Vec3f(-5, 0.5, -1), Quat1f(-M_PI, Vec3f(0, 1, 0)).toMatrix3x3()));
+	//vehicleTransforms.push_back(Transform3f(Vec3f(-10, 0.5, -1), Quat1f(M_PI, Vec3f(0, 1, 0)).toMatrix3x3()));
 
-	configCar->varVehiclesTransform()->setValue(vehicleTransforms);
+	//configCar->varVehiclesTransform()->setValue(vehicleTransforms);
 
 	auto multibody = scn->addNode(std::make_shared<MultibodySystem<DataType3f>>());
 	configCar->connect(multibody->importVehicles());
@@ -94,17 +92,17 @@ std::shared_ptr<SceneGraph> creatCar()
 	plane->stateTriangleSet()->connect(configCar->inTriangleSet());
 	plane->stateTriangleSet()->connect(multibody->inTriangleSet());
 
- 	//auto mapper = std::make_shared<DiscreteElementsToTriangleSet<DataType3f>>();
- 	//configCar->stateTopology()->connect(mapper->inDiscreteElements());
- 	//configCar->graphicsPipeline()->pushModule(mapper);
+ 	auto mapper = std::make_shared<DiscreteElementsToTriangleSet<DataType3f>>();
+ 	configCar->stateTopology()->connect(mapper->inDiscreteElements());
+ 	configCar->graphicsPipeline()->pushModule(mapper);
  
- 	//auto sRender = std::make_shared<GLSurfaceVisualModule>();
- 	//sRender->setColor(Color(0.3f, 0.5f, 0.9f));
- 	//sRender->setAlpha(0.8f);
- 	//sRender->setRoughness(0.7f);
- 	//sRender->setMetallic(3.0f);
- 	//mapper->outTriangleSet()->connect(sRender->inTriangleSet());
- 	//configCar->graphicsPipeline()->pushModule(sRender);
+ 	auto sRender = std::make_shared<GLSurfaceVisualModule>();
+ 	sRender->setColor(Color(0.3f, 0.5f, 0.9f));
+ 	sRender->setAlpha(0.8f);
+ 	sRender->setRoughness(0.7f);
+ 	sRender->setMetallic(3.0f);
+ 	mapper->outTriangleSet()->connect(sRender->inTriangleSet());
+ 	configCar->graphicsPipeline()->pushModule(sRender);
 
 	return scn;
 }
