1f748bf66c7c3268915a2aae0f69e2137de75bcf
YuzhongGuo <1345656488@qq.com>
2026-05-21 17:05:37 +0800

    Add J15

---
+++ src/Dynamics/Cuda/RigidBody/J15.h
@@ -0,0 +1,44 @@
+/**
+ * Copyright 2024 Yuzhong Guo
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+#pragma once
+#include "RigidBody/ArticulatedBody.h"
+
+#include "STL/Pair.h"
+
+#include "Field/FilePath.h"
+#include "Topology/EdgeSet.h"
+
+#include "Field/VehicleInfo.h"
+
+namespace dyno
+{
+	template<typename TDataType>
+	class J15 : virtual public ArticulatedBody<TDataType>
+	{
+		DECLARE_TCLASS(J15, TDataType)
+	public:
+		typedef typename TDataType::Real Real;
+		typedef typename TDataType::Coord Coord;
+
+		J15();
+		~J15() override;
+
+	protected:
+		void resetStates() override;
+
+	};
+}
+
